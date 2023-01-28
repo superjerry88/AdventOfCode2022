0040c7a0a18abbc4c6631cb71b4e9db7277803d8
Jerry <superjerry88@hotmail.com>
2023-01-28 21:01:05 +0800

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,17 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var calories = new List<int> {0};
+
+foreach (var line in lines)
+{
+    if (line == "")
+    {
+        calories.Add(0);
+    }
+    else
+    {
+        calories[^1] += int.Parse(line);
+    }
+}
+
+Console.WriteLine($"Part 1: {calories.Max()}");
+Console.WriteLine($"Part 2: {calories.OrderByDescending(c => c).Take(3).Sum()}");
