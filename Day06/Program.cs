ddfaff8fadfd2f8b3900a405bb9a586d162bcae5
Jerry <superjerry88@hotmail.com>
2023-01-28 23:45:44 +0800

    Added Day06

---
+++ Day06/Program.cs
@@ -0,0 +1,14 @@
+﻿var line = File.ReadAllText("input.txt");
+
+Console.WriteLine($"Part1: {FindMarker(line,4)}");
+Console.WriteLine($"Part1: {FindMarker(line,14)}");
+
+static int FindMarker(string input, int packetSize)
+{
+    for (var i = 0; i < input.Length - packetSize; i++)
+    {
+        if (input.Skip(i).Take(packetSize).Distinct().Count() != packetSize) continue;
+        return i + packetSize;
+    }
+    return 0;
+}
